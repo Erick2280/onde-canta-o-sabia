af6f866b110927567c76a3f249f19f47b8d4adaa
Erick Almeida <erickingles@gmail.com>
2021-06-01 16:39:38 -0300

    Cria repositório

---
+++ Bolsa de Jacaré/Bolsa de Jacaré/Bolsa_de_Jacare_App.swift
@@ -0,0 +1,17 @@
+//
+//  Bolsa_de_Jacare_App.swift
+//  Bolsa de Jacaré
+//
+//  Created by Erick Almeida on 01/06/21.
+//
+
+import SwiftUI
+
+@main
+struct Bolsa_de_Jacare_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Bolsa de Jacaré/Bolsa de Jacaré/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Bolsa de Jacaré
+//
+//  Created by Erick Almeida on 01/06/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
